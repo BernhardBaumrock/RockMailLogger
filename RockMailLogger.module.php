88c4860428c2a3a607caecd01280cb60853f9e84
BernhardBaumrock <office@baumrock.com>
2019-08-22 17:13:32 +0200

    init

---
+++ RockMailLogger.module.php
@@ -0,0 +1,56 @@
+<?php namespace ProcessWire;
+class WireMailPropGetter extends WireMail {
+  public function getProps() {
+    return array_keys($this->mail);
+  }
+}
+class RockMailLogger extends WireData implements Module {
+  public static function getModuleInfo() {
+    return [
+      'title' => 'WireMail Mail Logger',
+      'version' => '0.0.1',
+      'summary' => 'Logs all send() operations to the PW logs',
+      'autoload' => true,
+      'singular' => true,
+      'icon' => 'stack-overflow',
+    ];
+  }
+
+  public function init() {
+    $this->addHookAfter("WireMail::send", $this, "LogMailSend");
+  }
+
+  /**
+   * Log this send action to logs
+   */
+  public function LogMailSend($event) {
+    $mail = $event->object;
+    $this->log($this->getDump($mail));
+  }
+
+  /**
+   * Get mail data as JSON
+   */
+  public function getJSON($mail) {
+    return json_encode($this->getArr($mail));
+  }
+
+  /**
+   * Get mail data array
+   */
+  public function getArr($mail) {
+    $props = new WireMailPropGetter();
+    $props = $props->getProps();
+
+    $arr = [];
+    foreach($props as $prop) $arr[$prop] = $mail->get($prop);
+    return $arr;
+  }
+
+  /**
+   * Get log string for PW logs
+   */
+  public function getDump($mail) {
+    return print_r($this->getArr($mail), 1);
+  }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+This module will log information about all mails that are sent wie WireMail to the PW logs
+
+![img](https://i.imgur.com/mxkMWS0.png)
